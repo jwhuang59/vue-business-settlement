f0df024fe60eb66956913d4112c03e302705b9b7
yinshipeng <yinshipeng@126.com>
2020-09-18 12:57:28 +0800

    app

--- src/app.vue
+++ src/app.vue
@@ -1,20 +1,50 @@
 <template>
     <div id="app">
-        <router-view></router-view>
+        <div :class="[$route.meta.style,'header']">
+            <van-icon name="arrow-left" class="icon_left" @click="back(-1)" v-show="!$route.meta.isBack"/>
+            <p>{{$route.meta.title}}</p>
+        </div>
+        <router-view />
     </div>
 </template>
 <style lang="scss" src="./assets/scss/app.scss"></style>
 <style scoped>
-#app {
-    height: 100%;
+.header{
+    width:100%;
+    height:60px;
+    background:#F8F8F8;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    position: relative;
 }
+.style{
+    background:#31CF6E;
+    color:#fff;
+}
+.style .icon_left{
+    color:#fff;
+}
+.icon_left{
+    position: absolute;
+    left:10px;
+    top:50%;
+    transform: translate(0,-50%);
+    font-size:22px;
+    color:#333
+}
+.header p{
+    font-size:16px;
+}
+
 </style>
 <script>
-/** 动态引入icon-css样式文件 */
-/* eslint-disable */
-if (process.env.NODE_ENV === 'development') {
-    import('@/assets/scss/iconfont/iconfont.css');
-} else {
-    import('@/assets/scss/iconfont/iconfont.css');
-}
+import { icon,Button } from 'vant';
+export default {
+    components:{
+        [icon.name]: icon,
+        [Button.name]: Button
+    },
+};
 </script>
